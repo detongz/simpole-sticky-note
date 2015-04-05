faa9c3f159fc92f2479ed950a578fb2b12875139
detong <detong.chang@foxmail.com>
2015-04-05 19:52:30 +0800

    database

---
+++ database.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import pymongo
+
+
+def database():
+    conn = pymongo.Connection("localhost", 27017)
+    mydb = conn.stickyNote
+
+    myusr = mydb.stickyNote
+    a = myusr.find()
+    # print(a)
+    for mya in a:
+        print(mya)
+
+
