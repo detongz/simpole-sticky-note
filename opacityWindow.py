e5492aa66b266f1d026bf739c30ffa3366cf4596
detong <detong.chang@foxmail.com>
2015-04-06 13:19:50 +0800

    opacityWindow

--- opacityWindow.py
+++ opacityWindow.py
@@ -1,29 +1,42 @@
 #!/usr/bin/python3
+# -*- coding: utf-8 -*-
 
-import sys
-from PyQt5.QtWidgets import QWidget, QApplication, QLabel, QSlider, QPushButton
-from PyQt5.QtCore import Qt
-from PyQt5.QtGui import QIcon
-from database import *
+from PyQt5.QtWidgets import QWidget, QSlider, QPushButton, QHBoxLayout, QVBoxLayout, QApplication
+from PyQt5.QtCore import Qt, QObject
 
 
 class window(QWidget):
     def __init__(self):
         super().__init__()
-        self.octSlider = QSlider(Qt.Horizontal, self)
-        self.quit = QPushButton(self)
         self.windowUi()
+        self.toolboxUi()
         self.myStyleSheet()
 
     def windowUi(self):
+        self.setWindowFlags(Qt.WindowStaysOnTopHint | Qt.FramelessWindowHint)
+        self.setGeometry(300, 0, 200, 500)
+        self.show()
+
+    def toolboxUi(self):
+        self.quit = QPushButton()
+        self.octSlider = QSlider(Qt.Horizontal)
         self.octSlider.setRange(40, 100)
         self.octSlider.setSingleStep(1)
         self.octSlider.setValue(100)
         self.octSlider.valueChanged.connect(self.changeOpacityValue)
-        self.setWindowFlags(Qt.WindowStaysOnTopHint | Qt.FramelessWindowHint)
-        self.setGeometry(300, 0, 200, 500)
-        self.show()
         self.quit.clicked.connect(self.close)  # 关闭窗口
+
+        hbox = QHBoxLayout()
+        hbox.addStretch(1)
+        hbox.addWidget(self.quit)
+        hbox.addWidget(self.octSlider)
+
+        self.vbox = QVBoxLayout()
+        self.vbox.addStretch(1)
+        self.vbox.addLayout(hbox)
+
+        # self.setLayout(self.vbox)
+        # self.show()
 
     def changeOpacityValue(self):
         self.setWindowOpacity(self.octSlider.value() * 0.01)
@@ -36,19 +49,6 @@ class window(QWidget):
         )  # stylesheet设置样式表 css
         # self.quit.setIcon(QIcon('close.png'))
 
-
-class tools(QWidget):
-    def __init__(self):
-        super().__init__()
-        self.toolsUi()
-        self.btnClose = QPushButton('close', self)
-
-    def toolsUi(self):
-        self.btnClose.setStyleSheet()
-
-
-if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    w = window()
-    sys.exit(app.exec_())
-    # database()
+    def mouseMoveEvent(self, QMouseEvent):
+        self.setMouseTracking(1)
+        self.setLayout(self.vbox)
